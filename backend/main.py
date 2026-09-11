251ac76cd4ec2315a7a7c7483e9b1728a6b6aa99
Vijaya130 <vijayagn36@gmail.com>
2026-09-12 01:28:25 +0530

    Fix API CORS for deployment

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,8 @@ import os
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5178"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
